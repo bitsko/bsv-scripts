998af47a86f5be515ae3f7b4040d23bb7b4f9a6d
bitsko <26992310+bitsko@users.noreply.github.com>
2022-04-18 17:40:17 -0700

    Update createraw_1.sh

--- raw/createraw_1.sh
+++ raw/createraw_1.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-ncli(){ "$HOME"/.novo-bitcoin/bin/novobitcoin-cli $1 $2 $3 $4 $5 $6; }
-export -f ncli
-jqs(){ jq "$1"<<<$(cat) | sed 's/[[]//g;s/[]]//g;s/"//g;s/[[:space:]]*$//g;s/^[[:space:]]*//g;s/[:]//g;s/,//g;s/[{]//g;s/[}]//g' | sed '/^$/d'; }
-createraw(){ bash<<<"ncli createrawtransaction '[{\"txid\":\"""$(ncli listunspent | jqs .[0].txid)""\",\"vout\":0}]' '{\"""$(ncli listunspent | jqs .[0].address)""\":1}' 0"; }
+ncli(){ "$HOME"/.novo-bitcoin/bin/novobitcoin-cli $1 $2 $3 $4 $5 $6; }; export -f ncli
+createraw(){ vout=0; amount=1
+bash<<<"ncli createrawtransaction '[{\"txid\":\"""$(ncli listunspent | jq -r .[$vout].txid)""\",\"vout\":$vout}]' '{\"""$(ncli listunspent | jq -r .[$vout].address)""\":$amount}' 0"; }
 firstPartRaw=$(createraw)
-echo "first part of raw tx:"
 echo "$firstPartRaw"
-echo ""
-echo "first part raw decoded:"
-ncli decoderawtransaction "$firstPartRaw"
